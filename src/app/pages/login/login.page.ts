4808ac1354a2ddd8a6f2389cfd72dd18176e6ded
TigselemaAlex <kaiser.alex1994@gmail.com>
2024-05-01 12:57:18 -0500

    starting project

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonInput,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [
+    IonInput,
+    IonCardContent,
+    IonCardTitle,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCard,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+  ],
+})
+export class LoginPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+    console.log('LoginPage ngOnInit');
+  }
+}
